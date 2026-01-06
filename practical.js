d43a65880af97f51e3725ba2c190d24eb0ff15d3
Your Name <your.email@example.com>
2026-01-06 13:51:57 +0530

    Add code no:1

--- practical.js
+++ practical.js
@@ -1,7 +1,13 @@
-REGULAR FUNCTION
-1.Write a regular function that takes two numbers and returns their sum.
+// REGULAR FUNCTION
+// 1.Write a regular function that takes two numbers and returns their sum.
 
-function sum(a,b){
-    return a+b
+// function sum(a,b){
+//     return a+b
+// }
+// console.log(sum(120,1))
+
+2.Create a regular function that prints “Hello World” when called.
+function hello(){
+    return "heloo world"
 }
-console.log(sum(120,1))
+console.log(hello())
