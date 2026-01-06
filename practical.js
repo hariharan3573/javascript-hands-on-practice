db2c06f8755e61d6fc14350acb5613f7f92567ee
Your Name <your.email@example.com>
2026-01-06 14:18:16 +0530

    Add code no:4

--- practical.js
+++ practical.js
@@ -1,6 +1,6 @@
 // REGULAR FUNCTION
-// 1.Write a regular function that takes two numbers and returns their sum.
 
+// 1.Write a regular function that takes two numbers and returns their sum.
 // function sum(a,b){
 //     return a+b
 // }
@@ -12,15 +12,23 @@
 // }
 // console.log(hello())
 
-3.Write a regular function that checks if a number is even or odd.
-function check(nums){
-    for(i=0;i<nums.length;i++){
-        if(nums[i]%2==0){
-        console.log("it's a even")
-    }else{
-        console.log("it's a odd")
-    }
-    }
+// 3.Write a regular function that checks if a number is even or odd.
+// function check(nums){
+//     for(i=0;i<nums.length;i++){
+//         if(nums[i]%2==0){
+//         console.log("it's a even")
+//     }else{
+//         console.log("it's a odd")
+//     }
+//     }
     
-}
-check([23,45,7,12])
+// }
+// check([23,45,7,12])
+
+FUNCTION EXPRESSION
+
+// 4. Create a function expression that multiplies two numbers.
+// let a=function(a,b){
+//     return a*b
+// }
+// console.log(a(10,2))
