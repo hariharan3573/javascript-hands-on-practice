fec7a8e7076e1c07b3b62345a279ec52b25e83d5
Your Name <your.email@example.com>
2026-01-06 14:01:25 +0530

    add code no:3

--- practical.js
+++ practical.js
@@ -6,8 +6,21 @@
 // }
 // console.log(sum(120,1))
 
-2.Create a regular function that prints “Hello World” when called.
-function hello(){
-    return "heloo world"
+// 2.Create a regular function that prints “Hello World” when called.
+// function hello(){
+//     return "heloo world"
+// }
+// console.log(hello())
+
+3.Write a regular function that checks if a number is even or odd.
+function check(nums){
+    for(i=0;i<nums.length;i++){
+        if(nums[i]%2==0){
+        console.log("it's a even")
+    }else{
+        console.log("it's a odd")
+    }
+    }
+    
 }
-console.log(hello())
+check([23,45,7,12])
