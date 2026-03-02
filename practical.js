94542a04a4885aa1ac99bc8b36a9846b5712a6f4
Your Name <your.email@example.com>
2026-03-02 19:09:31 +0530

    Reverse array

--- practical.js
+++ practical.js
@@ -436,4 +436,32 @@
 //     console.log("it's zero")
 // }
 
+//34.Reverse a number
 
+// let nums=[1,2,3,4];
+
+//console.log(nums.reverse())
+
+// for(i=0;i<nums.length;i++){
+//     for(j=i+1;j<nums.length;j++){
+//         if(nums[i]<=nums[j]){
+//             temp=nums[i];
+//             nums[i]=nums[j];
+//             nums[j]=temp
+//         }
+//     }
+// }
+
+// console.log(nums)
+
+// for(i=0;i<nums.length;i++){
+//     for(j=i+1;j<nums.length;j++){
+//         if(nums[i]<=nums[j]){
+//             nums[i]=nums[i]+nums[j];
+//             nums[j]=nums[i]-nums[j];
+//             nums[i]=nums[i]-nums[j];
+//         }
+//     }
+// }
+
+// console.log(nums)
