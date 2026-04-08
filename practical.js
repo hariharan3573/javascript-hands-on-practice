9ccc8cf4dcf99a83d3152ec9eee92eed06bb0f60
Your Name <your.email@example.com>
2026-04-08 14:36:39 +0530

    count even numbers in the array

--- practical.js
+++ practical.js
@@ -1007,3 +1007,15 @@
 // }
 
 // console.log(min,max)
+
+//66.//Count even numbers in an array.
+
+// let arr=[4,5,6,2,8];
+// let count=0;
+// for(let i=0;i<arr.length;i++){
+//   if(arr[i]%2==0){
+//     count++
+//   }
+// }
+
+// console.log(count)
